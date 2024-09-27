1e9e756422ce906627337ff01239d754e20698b4
KseniyaBabaryka-creator <kseniabobariko2005@gmail.com>
2024-09-27 14:36:57 +0200

    Server request added

--- js/script.js
+++ js/script.js
@@ -184,35 +184,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	}
 
-	new MenuCard(
-		"img/tabs/vegy.jpg",
-		"vegy",
-		"Меню 'Фитнес'",
-		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-		10,
-		'.menu .container',
-		'menu__item'
-	).render();
+	const getResource = async (url) => {
+		const res = await fetch(url);
 
-	new MenuCard(
-		"img/tabs/elite.jpg",
-		"elite",
-		"Меню 'Премиум'",
-		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-		20,
-		'.menu .container',
-		'menu__item'
-	).render();
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
 
-	new MenuCard(
-		"img/tabs/post.jpg",
-		"post",
-		'Меню "Постное"',
-		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-		25,
-		'.menu .container',
-		'menu__item'
-	).render();
+		return await res.json();
+	}
+
+	getResource('http://localhost:3000/menu')
+		.then(data => {
+			data.forEach(({img, altimg, title, descr, price}) => {
+				new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+			});
+		})
+
+	
 
 	//Forms
 
@@ -225,10 +214,22 @@ document.addEventListener('DOMContentLoaded', () => {
 	};
 
 	forms.forEach(item => {
-		postData(item);
+		bindPostData(item);
 	});
 
-	function postData(form){
+	const postData = async (url, data) => {
+		const res = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: data
+		});
+
+		return await res.json();
+	}
+
+	function bindPostData(form){
 		form.addEventListener('submit', (e) => {
 			e.preventDefault();
 
@@ -241,30 +242,25 @@ document.addEventListener('DOMContentLoaded', () => {
 			
 			form.insertAdjacentElement('afterend', statusMessage);
 
-			const request = new XMLHttpRequest();
-			request.open('POST', 'js/server.php');
+			
 
-			request.setRequestHeader('Content-type', 'application/json');
+			
 			const formData = new FormData(form);
 
-			const object = {};
-			formData.forEach((value, key) => {
-				object[key] = value;
-			});
+			const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-			const json = JSON.stringify(object);
-
-			request.send(json);
-
-			request.addEventListener('load', () => {
-				if (request.status === 200) {
-					console.log(request.response);
+			postData('http://localhost:3000/requests', json)
+			.then(data => {
+					console.log(data);
 					showThanksModal(message.success);
-					form.reset();
 					statusMessage.remove();
-					
-				} else showThanksModal(message.failure);
+			}).catch(() => {
+				showThanksModal(message.failure);
+			}).finally(() => {
+				form.reset();
 			})
+
+			
 		});
 	}
 
@@ -291,4 +287,6 @@ document.addEventListener('DOMContentLoaded', () => {
 		}, 4000);
 
 	}
+
+	
 });
